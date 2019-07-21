cb6d6e18109000ad04538c5728e71bfd0d8d78b0
sumitkanoje <sumitkanoje@gmail.com>
2019-07-22 00:25:32 +0530

    Enhancements

--- src/main/java/com/knoje/f4/Main.java
+++ src/main/java/com/knoje/f4/Main.java
@@ -159,7 +159,6 @@ public class Main extends Application {
 				st.setOnFinished(new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent actionEvent) {
-						createDraggableRectangle(110, 220, 1190, -320);
 						iv1.setFitWidth(100);
 					}
 				});
@@ -262,117 +261,6 @@ public class Main extends Application {
 
 	static class Wrapper<T> {
 		T value;
-	}
-
-	private Rectangle createDraggableRectangle(double x, double y, double width, double height) {
-		final double handleRadius = 2;
-
-		Rectangle rect = new Rectangle(x, y, width, height);
-
-		// top left resize handle:
-		Circle resizeHandleNW = new Circle(handleRadius, Color.RED);
-		// bind to top left corner of Rectangle:
-		resizeHandleNW.centerXProperty().bind(rect.xProperty().divide(2));
-		resizeHandleNW.centerYProperty().bind(rect.yProperty().divide(2));
-
-		// bottom right resize handle:
-		Circle resizeHandleSE = new Circle(handleRadius, Color.GREEN);
-		// bind to bottom right corner of Rectangle:
-		resizeHandleSE.centerXProperty().bind(rect.xProperty().add(rect.widthProperty()));
-		resizeHandleSE.centerYProperty().bind(rect.yProperty().add(rect.heightProperty()));
-
-		// move handle:
-		Circle moveHandle = new Circle(handleRadius, Color.GOLD);
-		// bind to bottom center of Rectangle:
-		moveHandle.centerXProperty().bind(rect.xProperty().add(rect.widthProperty().divide(2)));
-		moveHandle.centerYProperty().bind(rect.yProperty().add(rect.heightProperty()));
-
-		// force circles to live in same parent as rectangle:
-		rect.parentProperty().addListener((obs, oldParent, newParent) -> {
-			for (Circle c : Arrays.asList(resizeHandleNW, resizeHandleSE, moveHandle)) {
-				Pane currentParent = (Pane) c.getParent();
-				if (currentParent != null) {
-					currentParent.getChildren().remove(c);
-				}
-				((Pane) newParent).getChildren().add(c);
-			}
-		});
-
-		Wrapper<Point2D> mouseLocation = new Wrapper<>();
-
-		setUpDragging(resizeHandleNW, mouseLocation);
-		setUpDragging(resizeHandleSE, mouseLocation);
-		setUpDragging(moveHandle, mouseLocation);
-
-		resizeHandleNW.setOnMouseDragged(event -> {
-			if (mouseLocation.value != null) {
-				double deltaX = event.getSceneX() - mouseLocation.value.getX();
-				double deltaY = event.getSceneY() - mouseLocation.value.getY();
-				double newX = rect.getX() + deltaX;
-				if (newX >= handleRadius && newX <= rect.getX() + rect.getWidth() - handleRadius) {
-					rect.setX(newX);
-					rect.setWidth(rect.getWidth() - deltaX);
-				}
-				double newY = rect.getY() + deltaY;
-				if (newY >= handleRadius && newY <= rect.getY() + rect.getHeight() - handleRadius) {
-					rect.setY(newY);
-					rect.setHeight(rect.getHeight() - deltaY);
-				}
-				mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
-			}
-		});
-
-		resizeHandleSE.setOnMouseDragged(event -> {
-			if (mouseLocation.value != null) {
-				double deltaX = event.getSceneX() - mouseLocation.value.getX();
-				double deltaY = event.getSceneY() - mouseLocation.value.getY();
-				double newMaxX = rect.getX() + rect.getWidth() + deltaX;
-				if (newMaxX >= rect.getX()
-						&& newMaxX <= rect.getParent().getBoundsInLocal().getWidth() - handleRadius) {
-					rect.setWidth(rect.getWidth() + deltaX);
-				}
-				double newMaxY = rect.getY() + rect.getHeight() + deltaY;
-				if (newMaxY >= rect.getY()
-						&& newMaxY <= rect.getParent().getBoundsInLocal().getHeight() - handleRadius) {
-					rect.setHeight(rect.getHeight() + deltaY);
-				}
-				mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
-			}
-		});
-
-		moveHandle.setOnMouseDragged(event -> {
-			if (mouseLocation.value != null) {
-				double deltaX = event.getSceneX() - mouseLocation.value.getX();
-				double deltaY = event.getSceneY() - mouseLocation.value.getY();
-				double newX = rect.getX() + deltaX;
-				double newMaxX = newX + rect.getWidth();
-				if (newX >= handleRadius && newMaxX <= rect.getParent().getBoundsInLocal().getWidth() - handleRadius) {
-					rect.setX(newX);
-				}
-				double newY = rect.getY() + deltaY;
-				double newMaxY = newY + rect.getHeight();
-				if (newY >= handleRadius && newMaxY <= rect.getParent().getBoundsInLocal().getHeight() - handleRadius) {
-					rect.setY(newY);
-				}
-				mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
-			}
-
-		});
-
-		return rect;
-	}
-
-	private void setUpDragging(Circle circle, Wrapper<Point2D> mouseLocation) {
-
-		circle.setOnDragDetected(event -> {
-			circle.getParent().setCursor(Cursor.CLOSED_HAND);
-			mouseLocation.value = new Point2D(event.getSceneX(), event.getSceneY());
-		});
-
-		circle.setOnMouseReleased(event -> {
-			circle.getParent().setCursor(Cursor.DEFAULT);
-			mouseLocation.value = null;
-		});
 	}
 
 	private void setUpDragging(ImageView iv, Wrapper<Point2D> mouseLocation) {
